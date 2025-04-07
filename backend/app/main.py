bf09ec38a0b8df1648942e9f9639c04e3e415504
bakliwalvaibhav1 <vbakliwalcodes@gmail.com>
2025-04-07 18:07:21 -0500

    fix(backend): enable CORS to allow frontend requests from dev server

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routes import auth
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # React frontend dev URL (Vite)
+    # allow_origins=["http://localhost:3000"],  # React frontend dev URL (Docker)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():

87e1b6375ae5183226bf86f8a7591ac537dc3a8c
bakliwalvaibhav1 <vbakliwalcodes@gmail.com>
2025-04-08 00:54:32 -0500

    feat(frontend): add sidebar layout with routing to pages

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -1,12 +1,13 @@
 import {createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom";
-import App from "../App";
+// import App from "../App";
 import Login from "../pages/Login";
 import Dashboard from "../pages/Dashboard";
 import NotFound from "../pages/NotFound";
+import AppLayout from "../layout/AppLayout";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
-        <Route path="/" element={<App />}>
+        <Route path="/" element={<AppLayout />}>
             <Route index element={<Login />} />
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="*" element={<NotFound />} />
